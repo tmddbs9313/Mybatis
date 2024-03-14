ac90db4284aacdeefca3fd09b45a4d1ea58c17c4
choseungyoun <tmddbs9313@naver.com>
2024-03-14 17:11:28 +0900

    24.3.14

--- 01_lecture-source/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/common/SearchCriteria.java
+++ 01_lecture-source/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/common/SearchCriteria.java
@@ -1,12 +1,13 @@
 package com.ohgiraffers.common;
 
-public class SeatchCriteria {
+public class SearchCriteria {
 
-    private String condition;  //검색 기준(메뉴 이름 혹은 카테고리명)
-    private String value;  //검색어
-    public SeatchCriteria() {}
+    private String condition;       // 검색 기준(메뉴 이름 혹은 카테고리명)
+    private String value;           // 검색어
 
-    public SeatchCriteria(String condition, String value) {
+    public SearchCriteria() {}
+
+    public SearchCriteria(String condition, String value) {
         this.condition = condition;
         this.value = value;
     }
@@ -29,7 +30,7 @@ public class SeatchCriteria {
 
     @Override
     public String toString() {
-        return "SeatchCriteria{" +
+        return "SearchCriteria{" +
                 "condition='" + condition + '\'' +
                 ", value='" + value + '\'' +
                 '}';
--- 01_lecture-source/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
+++ 01_lecture-source/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
@@ -1,7 +1,154 @@
 package com.ohgiraffers.section01.xml;
+import com.ohgiraffers.common.MenuDTO;
+import com.ohgiraffers.common.SearchCriteria;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.ohgiraffers.common.Template.getSqlSession;
 
 public class MenuService {
 
-        public void selectMenuByPrice(int price) {
+    private DynamicSqlMapper mapper;
+
+    public void selectMenuByPrice(int price) {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+
+        /* 필기. 기본 자료형으로는 조건문의 값을 비교할 수 없으며 hashmap 의 key
+         *       혹은 dto 의 getter 를 이용하여 값을 확인 할 수 있다.
+         *  */
+        Map<String, Integer> map = new HashMap<>();
+        map.put("price", price);
+        // 우리가 값을 넘겨주면 그 범위에 해당하는 메뉴들을 조회할 것이다.
+        List<MenuDTO> menuList = mapper.selectMenuByPrice(map);
+
+        if(menuList != null && menuList.size() > 0) {
+            for(MenuDTO menu : menuList) {
+                System.out.println(menu);
+            }
+        } else {
+            System.out.println("검색 결과가 존재하지 않습니다...");
+        }
+
+        sqlSession.close();
+
+    }
+
+    public void searchMenu(SearchCriteria searchCriteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+
+        List<MenuDTO> menuList = mapper.searchMenu(searchCriteria);
+
+        if(menuList != null && menuList.size() > 0) {
+            for(MenuDTO menu : menuList) {
+                System.out.println(menu);
+            }
+        } else {
+            System.out.println("검색 결과가 존재하지 않습니다.");
+        }
+
+        sqlSession.close();
+
+    }
+
+    public void searchMenuBySupCategory(SearchCriteria searchCriteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+
+        List<MenuDTO> menuList = mapper.searchMenuBySupCategory(searchCriteria);
+
+        if(menuList != null && menuList.size() > 0) {
+            for(MenuDTO menu : menuList) {
+                System.out.println(menu);
+            }
+        } else {
+            System.out.println("검색 결과가 존재하지 않습니다..");
+        }
+
+        sqlSession.close();
+
+    }
+
+    public void searchMenuByRandomMenuCode(List<Integer> randomMenuCodeList) {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+
+        Map<String, List<Integer>> criteria = new HashMap<>();
+        criteria.put("randomMenuCodeList", randomMenuCodeList);
+
+        List<MenuDTO> menuList =  mapper.searchMenuByRandomMenuCode(criteria);
+
+        if(menuList != null && menuList.size() > 0) {
+            for(MenuDTO menu : menuList) {
+                System.out.println(menu);
+            }
+        } else {
+            System.out.println("검색 결과가 존재하지 않습니다...");
+        }
+
+        sqlSession.close();
+
+    }
+
+    public void searchMenuByCodeOrSearchAll(SearchCriteria searchCriteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+
+        List<MenuDTO> menuList = mapper.searchMenuByCodeOrSearchAll(searchCriteria);
+
+        if(menuList != null && menuList.size() > 0) {
+            for(MenuDTO menu : menuList) {
+                System.out.println(menu);
+            }
+        } else {
+            System.out.println("검색 결과가 존재하지 않습니다...");
+        }
+
+        sqlSession.close();
+    }
+
+    public void searchMenuByNameOrCategory(Map<String, Object> criteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+
+        List<MenuDTO> menuList = mapper.searchMenuByNameOrCategory(criteria);
+
+        if(menuList != null && menuList.size() > 0) {
+            for(MenuDTO menu : menuList) {
+                System.out.println(menu);
+            }
+        } else {
+            System.out.println("검색 결과가 존재하지 않습니다...");
+        }
+
+        sqlSession.close();
+
+    }
+
+    public void modifyMenu(Map<String, Object> criteria) {
+
+        SqlSession sqlSession = getSqlSession();
+        mapper = sqlSession.getMapper(DynamicSqlMapper.class);
+        //DML 구문 update,delete,insert 트랜젝션 제어 필요(commit,rollback)
+        int result = mapper.modifyMenu(criteria);
+
+        if(result > 0){
+            System.out.println("메뉴 정보 변경에 성공!!!");
+            sqlSession.rollback();
+        }else {
+            System.out.println("메뉴정보 변경 실패");
+            sqlSession.rollback();
+        }
+        sqlSession.close();
     }
 }
